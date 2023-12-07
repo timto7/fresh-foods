be05b7c974529cea28a577e60920b5c7c50ba0cc
Tim Knight <timmy.knight@gmail.com>
2023-12-07 22:08:04 +0000

    Refine basket and add clear all button

--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -1,25 +1,41 @@
 import BasketItem from '../components/BasketItem';
+import Button from '../components/Button';
 import ContinueShoppingButton from '../components/ContinueShoppingButton';
 import useBasketStore from '../stores/basket-store';
 
-const BasketHeader = () => {
+const BuyBox = () => {
   const basket = useBasketStore((state) => state.basket);
   const totalPrice = useBasketStore((state) => state.totalPrice);
 
-  if (!basket.length) return <p>There are no items in the basket.</p>;
-
-  return <p>Total: £{totalPrice}</p>;
+  return (
+    <div>
+      <p>Total: £{totalPrice}</p>
+      {basket.length > 0 && <Button>Proceed to Checkout</Button>}
+    </div>
+  );
 };
 
 const BasketList = () => {
   const basket = useBasketStore((state) => state.basket);
+  const clearBasket = useBasketStore((state) => state.clearBasket);
 
   return (
-    <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
-      {basket.map((basketItem) => (
-        <BasketItem key={basketItem.product.id} product={basketItem.product} />
-      ))}
-    </ul>
+    <div>
+      <h1>Shopping Basket</h1>
+      {basket.length > 0 ? (
+        <Button onClick={() => clearBasket()}>Clear the Basket</Button>
+      ) : (
+        <p>There are no items in the basket.</p>
+      )}
+      <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+        {basket.map((basketItem) => (
+          <BasketItem
+            key={basketItem.product.id}
+            product={basketItem.product}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
@@ -27,8 +43,10 @@ const Basket = () => {
   return (
     <>
       <ContinueShoppingButton />
-      <BasketHeader />
-      <BasketList />
+      <div style={{ display: 'grid', gridTemplateColumns: 'auto 200px' }}>
+        <BasketList />
+        <BuyBox />
+      </div>
     </>
   );
 };
