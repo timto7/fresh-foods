4960192d930440562255276f6c2c88e183552a8f
Tim Knight <timmy.knight@gmail.com>
2023-12-09 10:56:40 +0000

    Configure testing

--- jest.config.ts
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  setupFileAfterEnv: ['<rootDir>/jest-setup.ts'],
-};
---
+++ src/components/Button.spec.tsx
@@ -0,0 +1,11 @@
+// import { render, screen } from '@testing-library/react';
+
+// import Button from './Button';
+
+describe('Button', () => {
+  it('applies the default type of button', () => {});
+
+  it('applies the specific type if provided', () => {});
+
+  it('applied a valid attribute to the element', () => {});
+});
